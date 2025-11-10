3674e5bfe6cc97831f8bedbbb975165b33e178d0
reustech <reus.tech.sergi@gmail.com>
2025-11-10 18:57:39 +0100

    sdfsafd

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#667eea',
+        secondary: '#764ba2',
+      },
+      spacing: {
+        '128': '32rem',
+      },
+    },
+  },
+  plugins: [],
+}
